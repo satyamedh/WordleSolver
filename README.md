b535932b0e505e12b497a194e6929a017b58ede3
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2024-03-01 01:32:47 +0530

    Basic game implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# WordleSolver
+## just me trynna learn statistics ig
+
+## got the words from https://gist.github.com/dracos/dd0668f281e685bad51479e5acaadb93
+
---
+++ wordle_implementation.py
@@ -0,0 +1,58 @@
+import os
+from termcolor import colored, cprint
+import random
+
+PREFIX = "/mnt/c/Users/Satyamedh/PycharmProjects/WordleAI/"
+unsorted_words = open(PREFIX + "words.txt", "r").read().split("\n")
+print(len(unsorted_words))
+
+words = sorted(unsorted_words)
+# remove duplicates
+words = list(set(words))
+# remove empty strings
+words = [word for word in words if word]
+# sort
+words = sorted(words)
+
+HAX = True
+
+cprint("Start...\n", "red", "on_black")
+our_word = random.choice(words)
+if HAX: print(our_word)
+done = False
+history_of_prints = []
+
+counter = 0
+
+while not done:
+    word = input()
+    # check if the word is valid
+    if len(word) != 5:
+        print("Invalid word. Please enter a word of length 5.")
+        continue
+    counter += 1
+    curr_line = []
+    if word != our_word:
+        for i_char in range(len(our_word)):
+            if word[i_char] == our_word[i_char]:
+                curr_line.append(colored(word[i_char], "white", "on_green"))
+            elif word[i_char] in our_word:
+                curr_line.append(colored(word[i_char], "white", "on_yellow"))
+            else:
+                curr_line.append(colored(word[i_char], "white", "on_grey"))
+        history_of_prints.append(''.join(curr_line))
+    else:
+        done = True
+        history_of_prints.append(colored(word, "white", "on_green"))
+        history_of_prints.append(f"Congrats! it took you {counter} tries")
+    # we'll be clearing the screen with newlines and reprinting the history of prints cuz fkin pycharm doesn't support clearing the screen
+    print("\n" * 50)
+
+    # reprint
+    for line in history_of_prints:
+        print(line)
+
+
+
+
+
