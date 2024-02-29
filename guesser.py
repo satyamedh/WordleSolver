6a173a1dbb453ca511ac46b11bf623fe46dfcb6d
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2024-03-01 03:42:34 +0530

    Bug fix: include green while checking for dupes in gray

--- guesser.py
+++ guesser.py
@@ -21,7 +21,6 @@ lifetime_green = "_____"
 lifetime_yellow = []
 lifetime_gray = []
 while not done:
-
     # ask for the green words
     green_words = input("Enter the green words(Example: if l and r are green in learn, l__r_. empty if none): ")
     # make sure the input is either empty or has 5 characters
@@ -44,8 +43,6 @@ while not done:
     # regex building. just replace the _ with .
     green_re = f'^{green_words.replace("_", ".")}'
 
-    counter = 0
-
     # for green, update only if the character is not _
     if green_words != "":
         for i in range(5):
@@ -54,22 +51,19 @@ while not done:
     lifetime_yellow = nuke_duplicates(lifetime_yellow + list(yellow_words))
     lifetime_gray = nuke_duplicates(lifetime_gray + list(gray_words))
 
-    # if something is in both yellow and gray, remove it from gray
-    for char in lifetime_yellow:
+    # if something is in green/yellow and gray, remove it from gray
+    for char in lifetime_yellow + list(lifetime_green):
         if char in lifetime_gray:
             lifetime_gray.remove(char)
 
 
     # filter the words
     for word in words:
-        # print only first 10
-        if counter == 10:
-            break
         green_result = (re.findall(green_re, word) if green_words else True)
         yellow_result = (all([char in word for char in lifetime_yellow]) if lifetime_yellow else True)
         gray_result = (any([char in word for char in lifetime_gray]) if lifetime_gray else False)
         if green_result and yellow_result and not gray_result:
-            counter += 1
-            print(word)
+            print(f'Try {word}')
+            break
 
     print("====================================")
